5fc064eb2a676bb1a1bf47cee954abd9df344436
kandy-peter <kandypeter03@gmail.com>
2022-03-28 16:02:12 +0200

    insert data in the database from data.sql file

--- schema.sql
+++ schema.sql
@@ -6,5 +6,5 @@ CREATE TABLE animals (
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL NOT NULL
+    weight_kg DECIMAL(10, 2) NOT NULL
 );
