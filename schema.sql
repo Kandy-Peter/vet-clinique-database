2ff5d67af07a2c6b0802fa6da47f5920adb7f115
kandy-peter <kandypeter03@gmail.com>
2022-03-30 09:45:41 +0200

    create the species table

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
@@ -14,7 +14,15 @@ ALTER TABLE animals ADD COLUMN species varchar(50);
 /* Create the owners table */
 
 CREATE TABLE owners (
-    id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     full_name VARCHAR(100) NOT NULL,
-    age INT NOT NULL
+    age INT NOT NULL,
+    PIRMARY KEY (id)
 );
+
+-- Create the species Tbale
+
+CREATE TABLE species (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+)
