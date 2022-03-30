f927567df20dd1e0b5a9de10089f99db51206b2f
kandy-peter <kandypeter03@gmail.com>
2022-03-30 10:02:42 +0200

    add an auto incremented id

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
@@ -14,15 +14,14 @@ ALTER TABLE animals ADD COLUMN species varchar(50);
 /* Create the owners table */
 
 CREATE TABLE owners (
-    id INT NOT NULL AUTO_INCREMENT,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     age INT NOT NULL,
-    PIRMARY KEY (id)
 );
 
 -- Create the species Tbale
 
 CREATE TABLE species (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
 )
